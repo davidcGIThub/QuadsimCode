3ced9608d90880d6f243abb5c0c087aaf33f4d1f
David <davidc@linuxpopper.org>
2020-06-14 16:05:06 -0600

    ugh

--- HW7/templateMatching.py
+++ HW7/templateMatching.py
@@ -21,8 +21,9 @@ for i in range(0, nlabels - 1):
         filtered_binary_map[labels == i + 1] = 255
 
 #produce image with background removed
-# binary_filtered_map_color = cv2.cvtColor(filtered_binary_map, cv2.COLOR_GRAY2BGR)
-# image_background_removed = cv2.bitwise_and(img_color, binary_filtered_map_color)
+binary_filtered_map_color = cv2.cvtColor(filtered_binary_map, cv2.COLOR_GRAY2BGR)
+masked_color_image = cv2.bitwise_and(img_color, binary_filtered_map_color)
+masked_gray_image = cv2.bitwise_and(img_gray,filtered_binary_map)
 
 #get contour around objects in view
 image, contours, hierarchy = cv2.findContours(filtered_binary_map,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -38,48 +39,47 @@ for cnt in contours:
     count = count + 1
 
 x,y,w,h = cv2.boundingRect(contours[best])
-template = filtered_binary_map[y:y+h,x:x+w]
-(tH, tW) = template.shape[::-1]
+cropped_masked_color_image = masked_color_image[y:y+h,x:x+w]
+cropped_gray_image = img_gray[y:y+h,x:x+w]
+cropped_color_image = img_color[y:y+h,x:x+w]
+cropped_filtered_binary_map = filtered_binary_map[y:y+h,x:x+w]
+#template = cv2.Canny(cropped_masked_gray_image,100,255)
+template = cropped_color_image
+(ht, wt) = cropped_gray_image.shape[::-1]
+mask = cropped_filtered_binary_map
 
 #load scene to check
-path_image = "/home/david/Videos/quad_unwashed/out1.png"
-image_scene = cv2.imread(path_image,3)
+path_image_scene = "/home/david/Videos/quad_unwashed/out32.png"
+image_scene = cv2.imread(path_image_scene,3)
+image_scene_gray = cv2.cvtColor(image_scene, cv2.COLOR_BGR2GRAY) 
+#scene = cv2.Canny(image_scene_gray,200,250)
+scene = image_scene
 
-# # Apply template Matching
-# w, h = template.shape[::-1]
-# res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-# min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-# top_left = max_loc
-# bottom_right = (top_left[0] + w, top_left[1] + h)
-# cv2.rectangle(image_scene,top_left, bottom_right, 255, 2)
-# cv2.imshow('image', image_scene)
-# cv2.waitKey(0) 
+(hs, ws) = image_scene_gray.shape[::-1]
 
-found = None
-for scale in np.linspace(0.1, 1.0, 20)[::-1]:
-    # resize the image according to the scale, and keep track
+R = min([hs/ht , ws/wt])
+found = (None)
+cv2.imshow("template", template)
+cv2.waitKey(0)
+cv2.imshow("scene", scene)
+cv2.waitKey(0)
+num_sizes = 20
+
+for scale in np.linspace(1.0*R/num_sizes, R, num_sizes)[::-1]:
+    # resize the template according to the scale, and keep track
     # of the ratio of the resizing
-    resized = imutils.resize(img_gray, width = int(img_gray.shape[1] * scale))
-    r = img_gray.shape[1] / float(resized.shape[1])
-    # if the resized image is smaller than the template, then break
-    # from the loop
-    if resized.shape[0] < tH or resized.shape[1] < tW:
-        break
-    # detect edges in the resized, grayscale image and apply template
-    # matching to find the template in the image
-    #edged = cv2.Canny(resized, 50, 200)
-    result = cv2.matchTemplate(resized, template, cv2.TM_CCOEFF)
+    resized_template = imutils.resize(template, width = int(template.shape[1] * scale))
+    result = cv2.matchTemplate(scene, resized_template, cv2.TM_CCORR_NORMED, mask = mask)
     (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
-    # if we have found a new maximum correlation value, then update
-    # the bookkeeping variable
+    # if we have found a new maximum correlation value, then update the bookkeeping variable
     if found is None or maxVal > found[0]:
-        found = (maxVal, maxLoc, r)
-# unpack the bookkeeping variable and compute the (x, y) coordinates
-# of the bounding box based on the resized ratio
-(_, maxLoc, r) = found
-(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+        found = (maxVal, maxLoc, scale)
+    
+# unpack the bookkeeping variable and compute the (x, y) coordinates of the bounding box
+(maxVal , maxLoc, r) = found
+(startX, startY) = (maxLoc[0] , maxLoc[1])
+(endX, endY) = int(maxLoc[0] + wt * r), int(maxLoc[1] + ht * r)
 # draw a bounding box around the detected result and display the image
-cv2.rectangle(img_color, (startX, startY), (endX, endY), (0, 0, 255), 2)
-cv2.imshow("Image", img_color)
+cv2.rectangle(image_scene, (startX, startY), (endX, endY), (0, 0, 255), 2)
+cv2.imshow("Image", image_scene)
 cv2.waitKey(0)
